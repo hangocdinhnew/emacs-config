e557747238f5583bf982e2300fe43a6e43c3502c
hangocdinhnew <dinhndrwnew@gmail.com>
2025-07-25 21:41:30 +0700

    Some stuff.

--- install.sh
+++ install.sh
@@ -15,8 +15,7 @@ for FILE in "${FILES[@]}"; do
   SOURCE="$CONFIG_DIR/$FILE"
 
   if [ -e "$TARGET" ] || [ -L "$TARGET" ]; then
-    echo "Backing up existing $TARGET to $TARGET.bak"
-    mv "$TARGET" "$TARGET.bak"
+    mv -v "$TARGET" "$TARGET.bak"
   fi
 
   ln -sv "$SOURCE" "$TARGET"
