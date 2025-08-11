f598cf46b2a986666bdd241e93773f0570dfc63d
hangocdinhnew <dinhndrwnew@gmail.com>
2025-08-11 21:21:08 +0700

    Add CMake and Zig.

--- .emacs.rc/language.el
+++ .emacs.rc/language.el
@@ -6,8 +6,18 @@
   :ensure t
   :defer t)
 
+(use-package cmake-mode
+  :ensure t
+  :defer t)
+
+(use-package zig-mode
+  :ensure t
+  :defer t)
+
 (require 'rust-mode)
 (require 'lua-mode)
+(require 'cmake-mode)
+(require 'zig-mode)
 
 (add-to-list 'load-path "~/.emacs.rc/local/")
 (require 'c3-mode)
