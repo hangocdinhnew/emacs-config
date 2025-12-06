f2a1f32165db45710f8f2a1dc17c9306cc81899d
hangocdinhnew <dinhndrwnew@gmail.com>
2025-12-06 20:10:11 +0700

    Some change.

--- .emacs.custom.el
+++ .emacs.custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((cmake-tab-width . 4))))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
