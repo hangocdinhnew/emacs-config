8f91d693c22176eec520b9f5782ad29ecbc15650
hangocdinhnew <dinhndrwnew@gmail.com>
2026-04-23 18:23:54 +0700

    Meow mode.

--- .emacs
+++ .emacs
@@ -11,5 +11,4 @@
 
 (require 'use-package)
 
-(load "~/.emacs.rc/which-key.el")
 (load "~/.emacs.rc/keybindings.el")
--- .emacs.rc/packages.el
+++ .emacs.rc/packages.el
@@ -34,34 +34,16 @@
   :after (embark consult)
   :defer t)
 
-(use-package evil
+(use-package meow
   :ensure t
   :init
   :config
-  (evil-mode t))
+  (require 'meow)
+  (meow-global-mode 1))
 
 (use-package magit
   :ensure t
   :defer t)
-
-(use-package evil-snipe
-  :ensure t
-  :after evil
-  :config
-  (evil-snipe-mode +1)
-  (evil-snipe-override-mode +1))
-
-(use-package evil-collection
-  :ensure t
-  :after (evil)
-  :config
-  (evil-collection-init))
-
-(use-package evil-mc
-  :ensure t
-  :after (evil)
-  :config
-  (global-evil-mc-mode 1))
 
 (use-package projectile
   :ensure t
